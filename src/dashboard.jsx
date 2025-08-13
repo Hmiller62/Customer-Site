d4d89d8bd7ba6b4aec999e00218a773567859887
Hmiller62 <haydenmiller242@gmail.com>
2025-08-13 14:12:02 -0400

    Initial commit

---
+++ src/dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to your Dashboard!</h1>
+      <p>This is your first custom component.</p>
+    </div>
+  );
+}
